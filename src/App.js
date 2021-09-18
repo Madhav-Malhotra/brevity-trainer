d8be13c9709978548beb6324e248eb0625fda41a
Madhav Malhotra <47190635+Madhav-Malhotra@users.noreply.github.com>
2021-09-18 10:53:27 -0400

    Accidentally uploaded wrong project files

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
         <Route path="/" exact>
           <Home />
         </Route>
-        <Route path="/record">
+        <Route path="/train">
           <Exercise />
         </Route>
       </Switch>
--- src/components/Exercise.js
+++ src/components/Exercise.js
@@ -3,17 +3,21 @@ import { saveAs } from 'file-saver';
 import VideoRecorder from 'react-video-recorder';
 import { NavLink } from 'react-router-dom';
 
-import getData from './getter';
-
 export default function Exercise(props) {
+  const [turn, setTurn] = useState(0);
   const [permission, setPermission] = useState(false);
   const video = useRef();
-  const [question, setQuestion] = useState(getData('question'));
-  const [intro, setIntro] = useState(getData('intro'));
-  const [outro, setOutro] = useState(getData('outro'));
+  const num = Math.floor(Math.random() * randomPrompts.length);
+  const [prompt, setPrompt] = useState(randomPrompts[num]);
 
   const download = () => {
-    saveAs(video.current, 'for-my-bestie.mp4');
+    saveAs(video.current, 'brevity-recording.mp4');
+  };
+
+  const nextRound = () => {
+    let nextNum = turn + 1;
+    if (nextNum === round.length) nextNum = 0;
+    setTurn(nextNum);
   };
 
   return (
@@ -22,27 +26,19 @@ export default function Exercise(props) {
         <img src="./back.png" alt="Go back home"/>
       </NavLink>
       <div className="exercise-overlay py-5">
-        <div className="row max-width-custom mx-auto text-center">
-          <div className="col-12 mx-auto">
-            <p className="mb-0">Record a video. Start with the intro, answer the question, and end with the outro.</p>
-            <p className="mt-0 mb-4">P.S. A question with 'them' means your friend ;-)</p>
+        <div className="row max-width-custom mx-auto">
+          <div className="col-12 text-center mx-auto">
+            <p>{round[turn]}</p>
+            <h5 className="subtitle mb-4">Your prompt: {prompt}</h5>
           </div>
-          <div className="col-12 shuffle-group">
-            <h5 className="subtitle mb-4">Intro: {intro}</h5>
-          </div>
-          <div className="col-12 shuffle-group">
-            <h5 className="subtitle mb-4">Question: {question}</h5>
-          </div>
-          <div className="col-12 shuffle-group mb-2">
-            <h5 className="subtitle mb-4">Outro: {outro}</h5>
-          </div>
-          <div className="col-12 mt-4">
+          <div className="col-12">
             <VideoRecorder
               onRecordingComplete={videoBlob => {
                 video.current = videoBlob;
               }}
               replayVideoAutoplayAndLoopOff
               showReplayControls
+              onStopReplaying={nextRound}
               isFlipped
               onTurnOnCamera={() => setPermission(true)}
               isOnInitially={permission}
@@ -50,12 +46,44 @@ export default function Exercise(props) {
           </div>
           <div className="col-12 d-flex justify-content-center mt-5">
             <div class="btn-group" role="group" aria-label="Basic example">
-              <button type="button" class="btn btn-danger" onClick={() => window.location.reload()}>New Prompts</button>
+              <button type="button" class="btn btn-danger" onClick={() => window.location.reload()}>Switch Prompt</button>
               <button type="button" class="btn btn-success" onClick={download}>Download Video</button>
             </div>
+            </div>
+          <div className="col-12 my-4"><hr className="divider" /></div>
+          <div className="col-12 text-center mx-auto">
+            <textarea className="notes" placeholder="Add notes here..."></textarea>
           </div>
         </div>
       </div>
     </div>
   )
 }
+
+
+const randomPrompts = [
+  "Describe a surprising mistake you made recently.",
+  "Describe a guilt you still carry on.",
+  "Describe one of your happiest childhood memories.",
+  "What interview question do you dread being asked? Why?",
+  "You can put anything on a billboard. What do you put? Why?",
+  "Describe the last time a tiny thing irritated you.",
+  "Describe a problem in the world that makes you feel angry.",
+  "Explain one of your regretful past decisions?",
+  "Explain a misunderstanding you had with a friend.",
+  "Describe a strong memory from middle/high school",
+  "What advice would you give to yourself 5 years ago? Why?",
+  "Describe someone who always makes you feel good. How do they do it?",
+  "What's something you've been avoiding? Why?",
+  "Explain to a toddler how superstores work.",
+  "Explain to a toddler how planets work.",
+  "Explain to a toddler how cars work.",
+  "Describe a time you struggled to speak up in a group.",
+  "Explain to a toddler how wind works."
+];
+
+const round = [
+  "Take as long as you want.",
+  "60 seconds max",
+  "30 seconds max"
+];
